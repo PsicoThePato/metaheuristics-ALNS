5f475965ff874ae06d75b7b819486535d7916fcb
PsicoThePato <jairo.moutinho@edu.ufes.br>
2021-12-25 22:58:10 -0300

    :truck: old definition.py

---
+++ problems/definitions.py
@@ -0,0 +1,11 @@
+class BaseState():
+    """
+    Implements state representation for a given problem
+    """
+    def __init__(self):
+        self.state = None
+
+    def read_from_file(self, path: str) -> None:
+        """
+        Reads state from a file and represents it on object
+        """
