315badfafe15a6282a8bd51ced803e5ebda3f332
Lucas_Moraes_Wize <lucas.moraes@wizecompany.com.br>
2022-02-16 15:56:29 -0300

    Estudei mais um pouco o código e coloquei uns printes para evidenciar oq é lido do arquivo

---
+++ utils/crvp.py
@@ -0,0 +1,20 @@
+import sys
+import math
+
+# class crvp:
+
+# 	def __init__(
+# 		self,
+# 		nb_customers, 
+# 		truck_capacity, 
+# 		distance_matrix, 
+# 		distance_warehouses, 
+# 		demands,
+# 		nb_trucks
+# 	):
+#         Dnb_customers = , 
+# 		truck_capacity, 
+# 		distance_matrix, 
+# 		distance_warehouses, 
+# 		demands,
+# 		nb_trucks
