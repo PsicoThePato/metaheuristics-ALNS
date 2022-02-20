3f4c9652b10367c58c3f73a705b5356545bb5eef
PsicoThePato <jairo.moutinho@edu.ufes.br>
2022-02-20 12:21:04 -0300

    :wrench:

--- problems/definitions.py
+++ problems/definitions.py
@@ -1,11 +1,21 @@
-class ProblemDefinition():
+from dataclasses import dataclass
+
+from typing import Any
+
+
+@dataclass
+class ProblemDefinition:
     """
     Implements state representation for a given problem
     """
-    def __init__(self):
-        self.state = None
+    state: Any
+    _file_path: str
 
-    def read_from_file(self, path: str) -> None:
+    def __init__(self, path: str):
+        self.state = None
+        self._file_path = path
+
+    def read_from_file(self) -> None:
         """
         Reads state from a file and represents it on object
         """
